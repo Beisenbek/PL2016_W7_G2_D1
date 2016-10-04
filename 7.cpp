827fbb126bafe35edab8bb051846a1f2a74f2f18
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2016-10-04 12:40:58 +0600

    second

---
+++ 7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+
+	int n;
+	cin >> n;
+
+	int f[n+1];
+
+	f[0] = 0;
+	f[1] = 1;
+
+	for(int i = 2; i < n; ++i){
+		f[i] = f[i-1] + f[i-2];
+	}
+
+	cout << f[n];
+
+
+	return 0;
+}
